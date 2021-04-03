658172fcb3712d7bb24cbd1d784dff314c11355e
jonathan <jhector39@gmail.com>
2021-04-02 23:00:48 -0400

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
 // Your code here
+function saturdayFun (activity="roller-skate"){
+        return `This Saturday, I want to ${activity}!`
+    }
+
+function mondayWork (activity="go to the office"){
+    return `This Monday, I will ${activity}.`
+}
+
+function wrapAdjective(style=`*`){
+    return function (result=`a hard worker`){
+    return `You are ${style}${result}${style}!`
+    }
+}
+
+const Calculator = {
+    add: function(a,b){
+        return a+b;
+    },
+    subtract: function(c,d){
+        return c-d;
+    },
+    multiply:function(e,f){
+        return e*f;
+    },
+    divide:function(g,h){
+        return g/h;
+    }
+}
+
+function actionApplyer(start, array){
+    let a = start
+    for(let i=0; i<array.length; i++ ){
+        a=array[i](a)
+    }
+    return a
+}
